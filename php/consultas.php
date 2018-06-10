0bfeaf6a0c4a1d599b2225962033f5efa00e3300
dferreiror <34660343+dferreiror@users.noreply.github.com>
2018-06-11 00:24:45 +0200

    Add files via upload

--- php/consultas.php
+++ php/consultas.php
@@ -1,18 +1,18 @@
 <?php
-    require("ConexionBasesDatos.php");
-    $estado = $_GET['boton'];
-	$fecha = $_GET['fecha'];
+    require("conexionBasesDatos.php");
+    $estado = $_GET['boton'];/*Recibe la identificación del botón que se ha clickado en la página*/
+	$fecha = $_GET['fecha'];/*Fecha con el formato '2018-01-04' por si se consulta un día en concreto*/
 	switch ($estado) {
-		case '4Semanas':
+		case '4Semanas'://144xdias * 28 dias = 4032 se pide 5000 por si acaso 
 			$query = "SELECT * FROM `tiempo` ORDER by id DESC LIMIT 5000";
 			break;
-		case '6Meses': //144xdias * 30 dias * 6meses
+		case '6Meses': //144xdias * 30 dias * 6meses = 25920 se pide 27000 por si acaso
 			$query = "SELECT * FROM `tiempo` ORDER by id DESC LIMIT 27000";
 			break;
 		case 'anos':
 			$query = "SELECT * FROM tiempo ORDER BY id DESC";
 			break;
-		case '24Horas': //'2018-01-04';
+		case '24Horas': 
 			$query = "SELECT * from tiempo where SUBSTRING(fecha_hora, 1, 10) = '$fecha'";
 			break;
 		default:
@@ -23,12 +23,9 @@
 	$consulta = mysqli_query($conexion,$query);
 	if ($consulta === FALSE) {
 		echo 'Error de ejecución de la consulta.<br />';
-	} 
-	else 
-	{
+	} else { //SI TODO HA IDO BIEN
 		$datos = "";
-		while($fila = mysqli_fetch_assoc($consulta))
-		{			
+		while($fila = mysqli_fetch_assoc($consulta)){			
 		   $fecha = $fila['fecha_hora'];
 		   $temperatura = $fila['temperatura'];
 		   $presion = $fila['presion'];
@@ -37,6 +34,8 @@
 		   $viento_vel = $fila['viento_vel'];
 		   $lluvia = $fila['lluvia'];
 
+		   /*Se rellena el array datos con los valores de los campos de la tabla tiempo
+		   cada posición del array es una fila de la tabla y se separa con ";" y los valores de cada fila con "="*/
 		   $datos .= $fecha."=".$temperatura."=".$presion."=".$humedad."=".$viento_dir."=".$viento_vel."=".$lluvia.";";
 		}
 		echo $datos;
